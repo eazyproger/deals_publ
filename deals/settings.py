31ee750c06d3a0b8c958030bbf2ef8b71f346afd
easyproger <semen-medvedev@mail.ru>
2021-01-19 16:39:16 +0700

    9

--- deals/settings.py
+++ deals/settings.py
@@ -125,7 +125,6 @@ DISABLE_COLLECTSTATIC = 1
 ALLOWED_HOSTS = ['*']
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-STATIC_URL = '/static/'
 STATICFILES_DIRS = (
     os.path.join(BASE_DIR, 'static'),
 )
