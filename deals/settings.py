ad9511b5cff6e01ec5c7a8a90ff9ea25697bf79c
easyproger <semen-medvedev@mail.ru>
2021-01-19 16:12:31 +0700

    5

--- deals/settings.py
+++ deals/settings.py
@@ -122,14 +122,15 @@ USE_TZ = True
 STATIC_URL = '/static/'
 
 if os.getcwd() == '/app':
+    import dj_database_url
+    DATABASES = {
+        'default': dj_database_url.config(default='postgres://localhost')
+    }
+    SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
+    ALLOWED_HOSTS = ['*']
     BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-
-    # Static files (CSS, JavaScript, Images)
-    # https://docs.djangoproject.com/en/1.9/howto/static-files/
     STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
     STATIC_URL = '/static/'
-
-    # Extra places for collectstatic to find static files.
     STATICFILES_DIRS = (
         os.path.join(BASE_DIR, 'static'),
     )
